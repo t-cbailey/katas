db1f6cfc7ff6ca266487150e7a2e96e069994c96
t-cbailey <t.cbaileymusic@gmail.com>
2023-11-15 09:50:02 +0000

    looping version

--- binaryChop/binaryChop.test.ts
+++ binaryChop/binaryChop.test.ts
@@ -26,6 +26,19 @@ describe("binary chop", () => {
     test("edge cases and mixed lengths", () => {
       expect(binaryChop(55, generator(5, 1000))).toBe(50);
       expect(binaryChop(4000, generator(0, 5000))).toBe(4000);
+      expect(binaryChop(10, [5, 6, 7, 8, 10, 22])).toBe(4);
+      expect(binaryChop(5986, generator(0, 10000))).toBe(5986);
+    });
+  });
+  describe("purity", () => {
+    test("should not mutuate input", () => {
+      const int = 1;
+      const arr = [1, 2];
+      expect(binaryChop(int, arr)).not.toBe(int);
+      expect(binaryChop(int, arr)).not.toBe(arr);
+      binaryChop(int, arr);
+      expect(arr).toEqual([1, 2]);
+      expect(int).toEqual(1);
     });
   });
 });
--- binaryChop/binaryChop.ts
+++ binaryChop/binaryChop.ts
@@ -2,24 +2,40 @@
 //binary chop;
 //take a sorted array and return the index of int
 
+//** Version 1 - recursive **//
+
 export function binaryChop(int: number, arr: number[]) {
   const midPoint = Math.floor(arr.length / 2);
-  let result;
 
   if (arr.length < 1) {
-    result = -1;
-    return result;
+    return -1;
   }
 
   if (arr[midPoint] === int) {
-    result = midPoint;
+    return midPoint;
   } else if (arr[midPoint] < int) {
     for (let i = midPoint; i < arr.length; i++) {
-      if (arr[i] === int) result = i;
+      if (arr[i] === int) return i;
     }
   } else {
     return binaryChop(int, arr.slice(0, midPoint));
   }
-
-  return result;
 }
+
+//** Version 2- looping  **//
+
+// export function binaryChop(int: number, arr: number[]) {
+//   let tempArr = [...arr];
+
+//   while (tempArr.length > 0) {
+//     const midPoint = Math.floor(tempArr.length / 2);
+
+//     for (let i = midPoint; i < tempArr.length; i++) {
+//       if (tempArr[i] === int) {
+//         return i;
+//       }
+//     }
+//     tempArr = tempArr.slice(0, midPoint);
+//   }
+//   return -1;
+// }
