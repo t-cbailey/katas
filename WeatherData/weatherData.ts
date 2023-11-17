67ebe54c39548f94d35830d18f0b29d712724843
t-cbailey <t.cbaileymusic@gmail.com>
2023-11-17 11:00:22 +0000

    completed part 1

--- WeatherData/weatherData.ts
+++ WeatherData/weatherData.ts
@@ -9,7 +9,7 @@ async function displayWeatherData() {
   let i = 91;
 
   while (i < data.length - 90) {
-    const sliced = data.slice(i, i + 20);
+    const sliced = data.slice(i, i + 14);
     splitData.push([sliced]);
     i += 90;
   }
@@ -23,19 +23,19 @@ async function displayWeatherData() {
       });
   });
 
-  console.log(splitData);
-
   const smallestDelta = [0, null];
 
   splitData.forEach((day) => {
-    const delta = day[1] - day[2];
+    const dayHigh = day[1].replace(/\*/, "");
+    const dayLow = day[2].replace(/\*/, "");
+    const delta = dayHigh - dayLow;
     if (delta < smallestDelta[1] || smallestDelta[1] === null) {
       smallestDelta[0] = day[0];
       smallestDelta[1] = delta;
     }
   });
 
-  return smallestDelta;
+  return `The day with the smallest temp variation is day ${smallestDelta[0]} with a delta of ${smallestDelta[1]} degrees.`;
 }
 
 displayWeatherData().then((res) => {
