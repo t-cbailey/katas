18aa96e9881722073c4945f69c0988889b84e6c2
t-cbailey <t.cbaileymusic@gmail.com>
2023-11-17 11:56:28 +0000

    completed DRY version

--- WeatherData/weatherData.ts
+++ WeatherData/weatherData.ts
@@ -74,3 +74,50 @@ async function displayFootballData() {
 displayFootballData().then((res) => {
   console.log(res);
 });
+
+//D.R.Y version//
+
+async function findSmallestDelta(filePath, col1, col2) {
+  const data = await fsPromises.readFile(filePath, "utf-8");
+  const type = filePath.match(/(?<=\/)\w+/gi).toString();
+
+  let splitData = data.split("\n");
+  splitData = splitData
+    .map((arr) => {
+      return arr.trim().replaceAll(/\s+/g, ",").split(",");
+    })
+    .slice(1, splitData.length);
+
+  const smallestDelta = [0, null];
+
+  splitData.forEach((arr) => {
+    if (arr.length > 1) {
+      const high = arr[col1];
+      const low = arr[col2];
+      let delta = high - low;
+      if (delta < 0) {
+        delta = -delta;
+      }
+      if (delta < smallestDelta[1] || smallestDelta[1] === null) {
+        smallestDelta[0] = type === "football" ? arr[1] : arr[0];
+        smallestDelta[1] = delta;
+      }
+    }
+  });
+
+  return `The ${
+    type == "football" ? "team" : "day"
+  } with the smallest variation in ${
+    type == "football" ? "goals" : "temperature"
+  } is ${smallestDelta[0]} with a delta of ${smallestDelta[1]} ${
+    type == "football" ? "goals" : "degrees"
+  }.`;
+}
+
+findSmallestDelta("./football.dat", 6, 8).then((res) => {
+  console.log(res);
+});
+
+findSmallestDelta("./weather.dat", 1, 2).then((res) => {
+  console.log(res);
+});
